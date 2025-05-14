16ce493e1ebd99a8572e881f70b7bb167ee2a593
Salma Arbaoui <salma.arbaoui15@gmail.com>
2025-05-14 09:33:09 +0200

    creation models

--- candidats/models.py
+++ candidats/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Candidat(models.Model):
+    nom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    cv = models.TextField(blank=True)
+    competences = models.TextField()
+
+    def __str__(self):
+        return self.nom
--- recruteurs/models.py
+++ recruteurs/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Candidat(models.Model):
+    nom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    cv = models.TextField(blank=True)
+    competences = models.TextField()
+
+    def __str__(self):
+        return self.nom
+
+class Recruteur(models.Model):
+    nom_entreprise = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    poste = models.CharField(max_length=100)
+    candidats_vus = models.ManyToManyField(Candidat, blank=True, related_name="recruteurs_interesses")
+
+    def __str__(self):
+        return f"{self.nom_entreprise} - {self.poste}"
